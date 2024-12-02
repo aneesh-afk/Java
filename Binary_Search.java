28f669f0666ffcc3b21684d856d539192313e770
aneesh-afk <aneeshsuvarna2006@gmail.com>
2024-12-02 21:28:24 +0530

    Binary search

---
+++ Binary_Search.java
@@ -0,0 +1,39 @@
+package Java;
+import java.util.Scanner;
+
+public class Binary_Search {
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter number of elements");
+        int n=scanner.nextInt();
+        System.out.println("Enter elements");
+        int []arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=scanner.nextInt();
+        }
+        System.out.println("Enter element to be searched");
+        int x=scanner.nextInt();
+        int index=Binary(arr,n,x);
+        System.out.println(index);
+    }
+    public static int Binary(int[] arr,int n,int x)
+    {
+        int ind=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==x)
+            {
+                ind=i;
+                break;
+            }
+            else
+            {
+                ind=-1;
+            }
+        }
+        return ind;
+    }
+
+}
